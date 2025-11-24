b0317062363917cb5766975c05f6daab48e97e66
temkinsx <temkinsx@gmail.com>
2025-11-24 16:20:20 +0300

    deleted ToDoList

--- src/practice8/ToDoList/src/main/java/ToDoListApplication.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = {"application", "domain", "infrastructure"})
-public class ToDoListApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ToDoListApplication.class, args);
-    }
-}
